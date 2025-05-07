b75dda62cde529163a65dcbda12ac135148218ef
Mifrhmh-git <miftahurrhmh461@gmail.com>
2025-05-07 14:30:40 +0700

    Add initial dashboard layout with Bootstrap integration and team section

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,393 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>{{ $title }}</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+
+  <style>
+    .card-img-top {
+      aspect-ratio: 3 / 4;
+      object-fit: cover;
+    }
+  </style>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Bootslander
+  * Template URL: https://bootstrapmade.com/bootslander-free-bootstrap-landing-page-template/
+  * Updated: Aug 07 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body class="index-page">
+
+    <header id="header" class="header d-flex align-items-center fixed-top">
+        <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+            <a href="index.html" class="logo d-flex align-items-center">
+                <img src="assets/img/logo-intership.png" alt="">
+            </a>
+
+            <nav id="navmenu" class="navmenu">
+                <ul>
+                <li><a href="#beranda">Beranda</a></li>
+                <li><a href="#tentang">Tentang</a></li>
+                <li><a href="#fitur">Fitur</a></li>
+                <li><a href="#team">Team</a></li>
+                <li class="dropdown"><a class="getstarted scrollto" href="#"><span>Masuk</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#" class="text-dark fw-medium fs-13">CDC Login</a></li>
+                        <li><a href="#" class="text-dark fw-medium fs-13">Masuk Industri</a></li>
+                        <li><a href="#" class="text-dark fw-medium fs-13">Masuk Mahasiswa</a></li>
+                        <li><a href="#" class="text-dark fw-medium fs-13">Masuk Dosen</a></li>
+                    </ul>
+                </li>
+                    </ul>
+                </li>
+                </ul>
+                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+            </nav>
+
+        </div>
+    </header>
+
+
+    <main class="main">
+
+    <section id="beranda" class="hero section dark-background">
+        <div class="container">
+            <div class="row gy-4 justify-content-between">
+                <div class="col-lg-4 order-lg-last hero-img" data-aos="zoom-out" data-aos-delay="100">
+                    <img src="assets/img/myinternship-hero.png" class="img-fluid animated" alt="">
+                </div>
+                <div class="col-lg-6  d-flex flex-column justify-content-center" data-aos="fade-in">
+                    <h1>Bangun Halaman Arahan Anda Dengan <span>MyInternship</span></h1>
+                    <p>Mengelola Pembelajaran Industri melalui Magang Terstruktur</p>
+
+                </div>
+            </div>
+        </div>
+
+        <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
+
+            <defs>
+            <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path>
+            </defs>
+
+            <g class="wave1">
+            <use xlink:href="#wave-path" x="50" y="3"></use>
+            </g>
+
+            <g class="wave2">
+            <use xlink:href="#wave-path" x="50" y="0"></use>
+            </g>
+
+            <g class="wave3">
+            <use xlink:href="#wave-path" x="50" y="9"></use>
+            </g>
+        </svg>
+    </section>
+
+
+    <section id="clients" class="clients clients">
+      <div class="container">
+          <div class="row">
+              <!-- Tut Wuri -->
+              <div class="col-lg-2 col-md-4 col-6">
+                  <img src="assets/img/TUT HURI HANDAYANI.png " class="img-fluid aos-init aos-animate" alt="" data-aos="zoom-in" data-aos-delay="100">
+              </div>
+
+              <!-- Polibatam -->
+              <div class="col-lg-2 col-md-4 col-6">
+                  <img src="assets/img/Polibatam.png" class="img-fluid aos-init aos-animate" alt="" data-aos="zoom-in" data-aos-delay="200">
+              </div>
+
+              <!-- Vokasi -->
+              <div class="col-lg-2 col-md-4 col-6">
+                  <img src="assets/img/Vokasi Indonesia.png" class="img-fluid aos-init aos-animate" alt="" data-aos="zoom-in" data-aos-delay="200">
+              </div>
+
+              <!-- International Labrour Organization -->
+              <div class="col-lg-2 col-md-4 col-6">
+                  <img src="assets/img/International Labrour Organization.png" class="img-fluid aos-init aos-animate" alt="" data-aos="zoom-in" data-aos-delay="100">
+              </div>
+
+              <!-- Skills For Prosperity -->
+              <div class="col-lg-2 col-md-4 col-6">
+                  <img src="assets/img/Skills For Prosperity.png" class="img-fluid aos-init aos-animate" alt="" data-aos="zoom-in" data-aos-delay="300">
+              </div>
+
+              <!-- Britsh Embassy Jakarta -->
+              <div class="col-lg-2 col-md-4 col-6">
+                  <img src="assets/img/Britsh Embassy Jakarta.png" class="img-fluid aos-init aos-animate" alt="" data-aos="zoom-in" data-aos-delay="400">
+              </div>
+
+          </div>
+      </div>
+    </section>
+
+
+    <section id="tentang" class="details section">
+        <div class="faq-container">
+            <div class="faq-header">
+                <div class="line"></div>
+                <h1>Tentang</h1>
+                <div class="line"></div>
+            </div>
+        <br>
+            <div class="row content">
+
+                <div class="col-lg-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="150">
+                    <p style="text-align: justify;">
+                    MyInternship adalah aplikasi manajer magang yang mengelola semua tahapan magang, mulai dari proses pendaftaran, pelaksanaan, hingga penilaian. Per 5 Oktober 2022, MyInternship telah digunakan oleh lebih dari 6000 mahasiswa yang melibatkan 300 dosen pembimbing industri dan 200 dosen pembimbing magang di Politeknik. MyInternship juga merupakan platform komunikasi antara mahasiswa, dosen dan dosen pembimbing.</p>
+                </div>
+
+                <div class="col-lg-6 pt-4 pt-lg-0 aos-init aos-animate" data-aos="fade-up" data-aos-delay="300">
+                    <p style="text-align: justify;">
+                    Perguruan tinggi dapat menugaskan pengawas magang, serta industri juga dapat menugaskan dosen pembimbing untuk setiap mahasiswa di tempat magang mereka. Melalui MyInternship, akan lebih mudah bagi politeknik dan industri untuk memantau dan mengevaluasi magang mahasiswa. Kinerja siswa dapat dilaporkan secara berkala melalui laporan kinerja. Melalui laporan kinerja ini, dosen pembimbing magang dapat memantau kemajuan belajar mahasiswa selama magang.</p>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <section id="counts" class="counts">
+        <div class="container">
+            <div class="row">
+
+                <div class="image col-xl-5 d-flex align-items-stretch justify-content-center justify-content-xl-start aos-init aos-animate" data-aos="fade-right" data-aos-delay="150">
+                    <img src="assets/img/goals.png" alt="" class="img-fluid">
+                </div>
+
+                <div class="col-xl-7 d-flex align-items-stretch pt-4 pt-xl-0 aos-init aos-animate" data-aos="fade-left" data-aos-delay="300">
+                    <div class="content d-flex flex-column justify-content-center">
+
+                        <div class="row">
+
+                            <div class="col-md-6 d-md-flex align-items-md-stretch">
+                                <div class="count-box">
+                                    <i class="bi bi-mortarboard"></i>
+                                    <span data-purecounter-start="0" data-purecounter-end="5074" data-purecounter-duration="0" class="purecounter">5074</span>
+                                    <p><strong>Mahasiswa</strong> magang terdaftar</p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 d-md-flex align-items-md-stretch">
+                                <div class="count-box">
+                                    <i class="bi bi-building"></i>
+                                    <span data-purecounter-start="0" data-purecounter-end="1301" data-purecounter-duration="0" class="purecounter">1301</span>
+                                    <p><strong>Industri</strong> Terlibat untuk Program Magang Mahasiswa</p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 d-md-flex align-items-md-stretch">
+                                <div class="count-box">
+                                    <i class="bi bi-people"></i>
+                                    <span data-purecounter-start="0" data-purecounter-end="298" data-purecounter-duration="0" class="purecounter">298</span>
+                                    <p><strong>Dosen</strong> terlibat untuk membimbing siswa selama magang</p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 d-md-flex align-items-md-stretch">
+                                <div class="count-box">
+                                    <i class="bi bi-hospital"></i>
+                                    <span data-purecounter-start="0" data-purecounter-end="7" data-purecounter-duration="0" class="purecounter">7</span>
+                                    <p><strong>Universitas</strong> telah bekerja sama dengan kami</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section id="fitur" class="services">
+        <div class="container">
+
+            <div class="section-title aos-init aos-animate" data-aos="fade-up">
+                <img src="assets/img/Featurs.jpg" alt="" class="img-fluid">
+                <center> Gambaran Umum Sistem secara Keseluruhan</center>
+            </div>
+
+        </div>
+    </section>
+
+
+    <section id="fitur" class="fitur-unggulan">
+        <div class="container">
+
+            <div class="judul-fitur" data-aos="fade-up">
+                <div class="gambar-fitur">
+                    <img src="assets/img/Featurs 1.jpg" alt="Fitur Unggulan" class="img-fitur">
+                    <center>Gambaran Umum Sistem yang akan dibangun </center>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+    <div class="container">
+
+        <div class="card">
+
+            <div class="icon-wrapper">
+                <div class="icon lightbulb">💡</div>
+            </div>
+
+            <h3>Peluang Magang</h3>
+            <p>Siswa dapat mendaftar magang melalui penawaran magang atau kerja sama</p>
+
+        </div>
+
+        <div class="card">
+
+            <div class="icon-wrapper">
+                <div class="icon clipboard">📋</div>
+            </div>
+
+            <h3>Pendaftaran Magang</h3>
+            <p>Mahasiswa Terdaftar yang mengajukan magang melalui penawaran magang atau kerjasama perusahaan atau mandiri</p>
+        </div>
+
+        <div class="card">
+
+            <div class="icon-wrapper">
+                <div class="icon document">📄</div>
+            </div>
+
+            <h3>Administrasi Magang</h3>
+            <p>Mahasiswa dan Perusahaan mengelola Administrasi Magang seperti Kehadiran, Buku Catatan, dll.</p>
+        </div>
+
+    </div>
+
+
+                    </div>
+                </section>
+                <section id="details" class="details section">
+                  <!-- ======= About Us Section ======= -->
+                  <div class="faq-container">
+                    <div class="faq-header">
+                      <div class="line"></div>
+                      <h1>Tim</h1>
+                      <div class="line"></div>
+                    </div>
+                    <div class="container py-5">
+  <div class="row g-4">
+
+    <!-- Ulangi card ini sebanyak 6 kali -->
+    <div class="col-md-4">
+      <div class="card h-100">
+        <img src="assets/img/team/Shandy Sapta Wirayudha.jpg" class="card-img-top" alt="Profile">
+        <div class="card-body">
+          <h5 class="card-title">Shandy Sapta Wirayudha </h5>
+          <p class="card-text"><i>Kontribusi sebagai penguji​</i></p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Duplikat 5 kartu lagi -->
+    <div class="col-md-4">
+      <div class="card h-100">
+        <img src="assets/img/team/Raihan Putra Atmaja .jpg" class="card-img-top" alt="Profile">
+        <div class="card-body">
+          <h5 class="card-title">Raihan Putra Atmaja  </h5>
+          <p class="card-text"><i>Kontribusi dalam pembuatan database, terutama ERD, diagram kelas, dan tabel​</i></p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Card ke-3 -->
+    <div class="col-md-4">
+      <div class="card h-100">
+        <img src="assets/img/team/Andri Yani Meuraxa .jpg" class="card-img-top" alt="Profile">
+        <div class="card-body">
+          <h5 class="card-title">Andri Yani Meuraxa </h5>
+          <p class="card-text"><i>Kontribusi sebagai UI merancang figma dan membantu pembuatan dokumen​</i></p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Card ke-4 -->
+    <div class="col-md-4">
+      <div class="card h-100">
+        <img src="assets/img/team/Miftahur Rahmah.jpg" class="card-img-top" alt="Profile">
+        <div class="card-body">
+          <h5 class="card-title">Miftahur Rahmah</h5>
+          <p class="card-text"><i>Kontribusi dalam implementasi laravel​</i></p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Card ke-5 -->
+    <div class="col-md-4">
+      <div class="card h-100">
+        <img src="assets/img/team/Alendea Resta Amaira .jpg" class="card-img-top" alt="Profile">
+        <div class="card-body">
+          <h5 class="card-title">Alendea Resta Amaira </h5>
+          <p class="card-text"><i>Kontribusi dalam pembuatan dokumen, desain, dan database​</i></p>
+        </div>
+      </div>
+    </div>
+    <!-- Card ke-6 -->
+    <div class="col-md-4">
+      <div class="card h-100">
+        <img src="assets/img/team/Rahel Tarigan .jpg" class="card-img-top" alt="Profile">
+        <div class="card-body">
+          <h5 class="card-title">Rahel Tarigan </h5>
+          <p class="card-text"><i>Kontribusi sebagai desain UX dan membantu dalam pembuatan dokumen​​</i></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+                        </div>
+                    </section>
+
+
+<div class="faq-container">
+  <div class="faq-header">
+ </div>
+  <link rel="stylesheet" href="contact.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <!-- Preloader -->
+  <div id="preloader"></div>
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
